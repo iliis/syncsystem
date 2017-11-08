b6d40e6fb362db4b7d9c7921329bafa053856ada
Samuel Bryner <samuelbryner@gmx.ch>
2017-11-08 17:20:02 +0100

    Plot full bag, including events. Track blinking LED.

--- event_visualizer/src/plot_bag.py
+++ event_visualizer/src/plot_bag.py
@@ -1,31 +1,223 @@
 #!/usr/bin/env python
 
+import itertools
+import datetime
+
 import rospy
 import rosbag
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
-import datetime
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
+
+
+def print_progress(current_t, start_t, end_t):
+    progress = (current_t.to_sec() - start_t) / (end_t-start_t)
+    print "\x1b[G{:3.1f} %".format(progress*100), 
 
 
 def stamp(ts):
     return (ts.secs % 1000) + ts.nsecs / 1000000000.0
 
-def timestamps_to_lines(ts, offset):
+def timestamps_to_lines(ts, offset=0):
     lines = []
     for t in ts:
-        s = stamp(t)
-        lines.append( ((s,offset),(s,offset+1)) )
+        lines.append( ((t,offset),(t,offset+1)) )
     return LineCollection(lines)
 
 
+def to_scaled_color(val, _min, _max):
+    c = (val-_min) / float(_max-_min)
+    if c > 1:
+        c = 1
+    elif c < 0:
+        c = 0
+    return ( c, 0, 0, 1)
+
+def to_lines(objs, get_t, get_col=None, offset=0):
+    lines = []
+    colors = []
+
+    def to_line(t):
+        return ((t,offset), (t,offset+1))
+
+    for obj in objs:
+        ls = get_t(obj)
+
+        if get_col:
+            col = get_col(obj)
+
+            if isinstance(ls, list):
+                colors.extend([col]*len(ls))
+            else:
+                colors.append(col)
+
+        if isinstance(ls, list):
+            lines.extend(map(to_line, ls))
+        else:
+            lines.append(to_line(ls))
+
+    return LineCollection(lines, colors=colors)
+
+def render_events(msgs, binsize, total_duration, scale_max_to=None):
+    bins_pos = []
+    bins_neg = []
+    cur_bin_t = None
+    count_pos = 0
+    count_neg = 0
+    first_t = None
+    for _, msg, t in msgs:
+        for e in msg.events:
+            if cur_bin_t is None:
+                cur_bin_t = e.ts
+                first_t = e.ts
+
+            while cur_bin_t+binsize < e.ts:
+                bins_pos.append(count_pos)
+                bins_neg.append(count_neg)
+                count_pos = 0
+                count_neg = 0
+                cur_bin_t += binsize
+
+            if e.polarity:
+                count_pos += 1
+            else:
+                count_neg += 1
+
+        print_progress(t, total_duration[0], total_duration[1])
+
+    if scale_max_to is not None:
+        s = scale_max_to / float(max(max(bins_pos), max(bins_neg)))
+
+        bins_pos = [b * s for b in bins_pos]
+        bins_neg = [b * s for b in bins_neg]
+
+    plt.plot(np.linspace(stamp(first_t), stamp(cur_bin_t), len(bins_pos)), bins_pos, 'blue')
+    plt.plot(np.linspace(stamp(first_t), stamp(cur_bin_t), len(bins_neg)), bins_neg, 'red')
+
+
+class SyncFrame:
+    def __init__(self, start_t, end_t, ros_start_t, ros_end_t):
+        self.start_t = start_t
+        self.end_t   = end_t
+        self.duration = end_t - start_t
+        self.center = (end_t + start_t) / 2
+
+        self.ros_start_t = ros_start_t
+        self.ros_end_t   = ros_end_t
+        #print self.duration*1000
+
+
 if __name__ == '__main__':
-    bag = rosbag.Bag('/home/samuel/rpg_syncsystem_local/bagfiles/short_jitter_all_2017-11-06-19-49-49.bag')
+    #bag = rosbag.Bag('/home/samuel/rpg_syncsystem_local/bagfiles/startup_sync_40fps_2017-11-07-12-46-09.bag')
+    #bag = rosbag.Bag('/home/samuel/rpg_syncsystem_local/bagfiles/blink_to_bluefox_per5_frames_2017-11-07-14-26-12.bag')
+    bag = rosbag.Bag('/home/samuel/rpg_syncsystem_local/bagfiles/blink_to_bluefox_60_300_frames_2017-11-08-17-15-14.bag')
+
     fig, ax = plt.subplots()
 
-    ax.add_collection(timestamps_to_lines([msg.header.stamp for topic, msg, t in bag.read_messages(topics=['/camera/image_raw'])], 0))
-    ax.add_collection(timestamps_to_lines([msg.ts           for topic, msg, t in bag.read_messages(topics=['/dvs/special_events'])], 1))
-    ax.add_collection(timestamps_to_lines([msg.header.stamp for topic, msg, t in bag.read_messages(topics=['/synchronized/camera/image_raw'])], 2))
-    ax.add_collection(timestamps_to_lines([msg.header.stamp for topic, msg, t in bag.read_messages(topics=['/cam_remapped_to_ev'])], 3))
+    start_t = bag.get_start_time()
+    end_t   = bag.get_end_time()
+
+    bridge = CvBridge()
+
+    #ax.add_collection(timestamps_to_lines([msg.header.stamp for topic, msg, t in bag.read_messages(topics=['/camera/image_raw'])], 0))
+    #ax.add_collection(timestamps_to_lines([msg.ts           for topic, msg, t in bag.read_messages(topics=['/dvs/special_events'])], 1))
+    #ax.add_collection(timestamps_to_lines([msg.header.stamp for topic, msg, t in bag.read_messages(topics=['/synchronized/camera/image_raw'])], 2))
+    #ax.add_collection(timestamps_to_lines([msg.header.stamp for topic, msg, t in bag.read_messages(topics=['/cam_remapped_to_ev'])], 3))
+
+
+    print "rendering special events..."
+
+    events = [(t, msg) for topic, msg, t in bag.read_messages(topics=['/dvs/special_events'])]
+
+    sync_frames = []
+    for i in range(len(events)-1):
+        if events[i][1].polarity == True and events[i+1][1].polarity == False:
+            sync_frames.append(SyncFrame(stamp(events[i][1].ts), stamp(events[i+1][1].ts), stamp(events[i][0]), stamp(events[i+1][0])))
+
+    durations = [f.duration for f in sync_frames]
+    #lines.set_color( [ (1,0,0,1), (1,0,0,0), (1,1,0,1) ] )
+
+
+
+    lines = []
+    colors = []
+    for f in sync_frames:
+        lines.append(f.start_t)
+        lines.append(f.end_t)
+
+        #colors.append( to_scaled_color(f.duration, 0.008, 0.009) )
+        #colors.append( to_scaled_color(f.duration, 0.008, 0.009) )
+        colors.append( (1,0,0,1) )
+        colors.append( (0,0,0,1) )
+
+    collection = timestamps_to_lines(lines, -1)
+    collection.set_color(colors)
+
+    ax.add_collection(collection)
+
+    print "rendering image frames..."
+
+    def mean_img_val(img):
+        cv_image = bridge.imgmsg_to_cv2(img) #, desired_encoding="passthrough")
+        return cv_image.mean()
+
+    #ax.add_collection(timestamps_to_lines([stamp(msg.header.stamp) for topic, msg, t in bag.read_messages(topics=['/camera/image_raw'])], 1))
+    ax.add_collection(to_lines(
+        [msg for topic, msg, t in bag.read_messages(topics=['/camera/image_raw'])],
+        lambda msg: stamp(msg.header.stamp),
+        #lambda msg: to_scaled_color(msg.exp_time_us, 8000, 9000),
+        lambda m: to_scaled_color(mean_img_val(m), 0.6, 3),
+        -2
+        ))
+
+    #ax.add_collection(timestamps_to_lines([stamp(msg.header.stamp) for topic, msg, t in bag.read_messages(topics=['/dvs/events'])], 2))
+
+    for topic, msg, t in bag.read_messages(topics=['/camera/capture_info', '/camera/image_raw']):
+        if topic == '/camera/capture_info':
+            #print msg.header.stamp, t.to_sec()-start_t, msg.exp_time_us
+            pass
+        elif topic == '/camera/image_raw':
+            print msg.header.stamp, mean_img_val(msg)
+
+
+    print "rendering events..."
+    render_events(bag.read_messages(topics=['/dvs/events']), rospy.Duration.from_sec(0.0001), (start_t, end_t), scale_max_to=1)
+    print ""
+
+    print "calculating offsets..."
+    last_special_event = None
+    last_image_transition = None
+    last_image_avg = None
+    for topic, msg, t in bag.read_messages(topics=['/camera/image_raw', '/dvs/special_events']):
+        if topic == '/dvs/special_events':
+            last_special_event = msg
+        elif topic == '/camera/image_raw':
+
+            avg = mean_img_val(msg)
+
+            if last_image_avg is not None and abs(last_image_avg[1] - avg) > 1.5:
+                if avg-last_image_avg[1] > 0:
+                    polarity = True
+                else:
+                    polarity = False
+
+                last_image_transition = (last_image_avg[0], msg.header.stamp, polarity)
+
+            last_image_avg = (msg.header.stamp, avg)
+
+        # align
+        if last_special_event is not None and last_image_transition is not None:
+            d1 = (last_image_transition[0] - last_special_event.ts).to_sec() * 1000
+            d2 = (last_image_transition[1] - last_special_event.ts).to_sec() * 1000
+            print "got match, delta =", d1, "ms to", d2, "ms", last_image_transition[2] == last_special_event.polarity
+            last_special_event = None
+            last_image_transition = None
+
+        #print_progress(t, start_t, end_t)
+
+    print "done"
 
     bag.close()
 
--- synchronizator/src/test_exp_time.py
+++ synchronizator/src/test_exp_time.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import datetime
+import math
 
 import rospy
 import dynamic_reconfigure.client
@@ -8,9 +9,13 @@ from sensor_msgs.msg import Image
 from dvs_msgs.msg import SpecialEvent, Event, EventArray
 from bluefox_ros.msg import CaptureInfo
 from std_srvs.srv import Trigger, TriggerResponse
+import message_filters
 
 
 
+# TODO: prevent changes to exposure time while waiting for previous change
+# TODO: timeout wait for exp time change
+# TODO: detect significant changes in timing offset (for example due to dropped frame)
 
 
 
@@ -28,7 +33,20 @@ class ExpTimeTest:
         self.configclient = dynamic_reconfigure.client.Client('/camera')
 
         rospy.Subscriber("/dvs/special_events", SpecialEvent, self.special_event_callback)
-        rospy.Subscriber("/camera/capture_info", CaptureInfo, self.image_callback)
+
+        #rospy.Subscriber("/camera/capture_info", CaptureInfo, self.image_callback)
+
+        capture_info = message_filters.Subscriber("/camera/capture_info", CaptureInfo)
+        camera_image = message_filters.Subscriber("/camera/image_raw", Image)
+
+        self.synced_cam_info = message_filters.TimeSynchronizer([capture_info, camera_image], 10)
+        self.synced_cam_info.registerCallback(self.image_callback)
+
+
+        self.capt_info_pub = rospy.Publisher("/synchronized/camera/capture_info", CaptureInfo, queue_size=2)
+        self.image_pub     = rospy.Publisher("/synchronized/camera/image_raw", Image, queue_size=2)
+
+
 
         rospy.Service('change_exp_time', Trigger, self.change_trigger)
 
@@ -37,8 +55,15 @@ class ExpTimeTest:
         self.currently_set_exp_time = 0
 
         # timestamp of first image received that matches new exposure time
-        self.first_good_syncframe = None
+        self.first_good_syncframe   = None
         self.first_good_cameraframe = None
+
+        self.first_good_sync_seq    = None
+        self.first_good_image_seq   = None
+        self.image_event_seq_offset = None
+
+        self.sync_timestamps = {}
+        self.image_queue = []
 
     def matches_expected_exposure_time(self, exp_time_us):
         return self.currently_set_exp_time - ExpTimeTest.TOLERANCE_US <= exp_time_us <= self.currently_set_exp_time + ExpTimeTest.TOLERANCE_US
@@ -46,29 +71,79 @@ class ExpTimeTest:
     def special_event_callback(self, event):
         if event.polarity == False:
             self.last_falling = event.ts
-        else:
-            #print "got sync frame (exp_time:", (event.ts - self.last_falling).to_sec()*1000*1000, ", ts:", event.ts.to_sec(), ")"
-            if self.first_good_syncframe is None and self.matches_expected_exposure_time((event.ts - self.last_falling).nsecs/1000):
-                #print("MATCH!")
+            return
 
-                # todo: which point in time to take? middle of exposure?
-                #self.first_good_syncframe = self.last_falling
-                self.first_good_syncframe = event.ts
-                self.check_first_good()
-
-    def image_callback(self, img):
-        #print "got camera frame (exp_time:", img.exp_time_us, ", ts:", img.header.stamp.to_sec(), ")"
-        if self.first_good_cameraframe is None and self.matches_expected_exposure_time(img.exp_time_us):
+        #print "got sync frame (exp_time:", (event.ts - self.last_falling).to_sec()*1000*1000, ", ts:", event.ts.to_sec(), ")"
+        if self.first_good_syncframe is None and self.matches_expected_exposure_time((event.ts - self.last_falling).nsecs/1000):
             #print("MATCH!")
-            self.first_good_cameraframe = img.header.stamp
+
+            # todo: which point in time to take? middle of exposure?
+            #self.first_good_syncframe = self.last_falling
+            self.first_good_syncframe = event.ts
+            self.first_good_sync_seq  = event.header.seq
             self.check_first_good()
+
+        if self.image_event_seq_offset is not None:
+            # middle of exposure
+            correct_ts = self.last_falling + (event.ts-self.last_falling)/2
+            self.sync_timestamps[int(math.floor(event.header.seq/2))] = correct_ts
+
+            self.check_pending_buffers()
+
+    def image_callback(self, info, img):
+
+        #print "got camera frame (exp_time:", img.exp_time_us, ", ts:", img.header.stamp.to_sec(), ")"
+        if self.first_good_cameraframe is None and self.matches_expected_exposure_time(info.exp_time_us):
+            #print("MATCH!")
+            self.first_good_cameraframe = info.header.stamp
+            self.first_good_image_seq   = img.header.seq
+            self.check_first_good()
+
+        if self.image_event_seq_offset is not None:
+            self.image_queue.append((info, img))
+            self.check_pending_buffers()
 
 
     def check_first_good(self):
         if self.first_good_syncframe and self.first_good_cameraframe:
-            print "found matching pair: time difference:", (self.first_good_cameraframe - self.first_good_syncframe).to_sec()*1000, "ms"
-
             # now keep track of this match-up...
+            self.image_event_seq_offset = int(math.floor(self.first_good_sync_seq/2)) - self.first_good_image_seq
+
+            print "found matching pair: time difference:", (self.first_good_cameraframe - self.first_good_syncframe).to_sec()*1000, "ms"
+            print "seq offset:", self.image_event_seq_offset
+
+
+    def check_pending_buffers(self):
+        if len(self.image_queue) == 0 or len(self.sync_timestamps) == 0:
+            return
+
+        i = 0
+        while i < len(self.image_queue):
+            info, img = self.image_queue[i]
+            expected_seq = img.header.seq + self.image_event_seq_offset
+
+            if expected_seq in self.sync_timestamps:
+
+                print "published synced image pair. offset:", (info.header.stamp-self.sync_timestamps[expected_seq]).to_sec()*1000, "ms"
+
+                info.header.stamp = self.sync_timestamps[expected_seq]
+                img .header.stamp = self.sync_timestamps[expected_seq]
+
+                self.image_pub.publish(img)
+                self.capt_info_pub.publish(info)
+
+                del self.image_queue[i]
+                del self.sync_timestamps[expected_seq]
+            else:
+                i += 1
+
+        if len(self.image_queue) > 2 and len(self.sync_timestamps) > 2:
+            print "WARNING: Matchup between images and sync events failed!"
+            for info, img in self.image_queue:
+                print "> IMAGE:", img.header.seq, img.header.seq+self.image_event_seq_offset
+
+            for seq, ts in self.sync_timestamps.iteritems():
+                print "> SYNC:", seq
 
 
     def change_trigger(self, reg):
@@ -83,8 +158,15 @@ class ExpTimeTest:
         print "changed exposure time to", self.config['exp_time']/1000, "ms"
         self.currently_set_exp_time = self.config['exp_time']
 
-        self.first_good_syncframe = None
+        self.first_good_syncframe   = None
         self.first_good_cameraframe = None
+
+        self.first_good_sync_seq    = None
+        self.first_good_image_seq   = None
+        self.image_event_seq_offset = None
+
+        self.sync_timestamps = {}
+        self.image_queue = []
 
         response = TriggerResponse()
         response.success = True
